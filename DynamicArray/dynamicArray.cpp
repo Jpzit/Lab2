9fc9649ab12945a51849635a28a996ae5ca4b1d8
Jpzit <jpto2000@hotmail.ca>
2015-01-30 10:58:02 -0500

    les 3 premiers test marche
    je suis rendu a setCapacite()

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,52 @@
 #include "dynamicArray.h"
+using namespace std;
+
+
+DynamicArray::DynamicArray()
+{
+	capacite = 100;
+	tabElement = new int[capacite];
+	for (int i = 0; i < capacite; i++)
+	{
+		tabElement[i] = 0;
+	}
+}
+
+DynamicArray::DynamicArray(const int& _capacite)
+{
+	capacite = _capacite;
+	tabElement = new int[capacite];
+	for (int i = 0; i < capacite; i++)
+	{
+		tabElement[i] = 0;
+	}
+}
+
+DynamicArray::~DynamicArray()
+{
+	delete[] tabElement;
+}
+
+int DynamicArray::getCapacite()
+{
+	return capacite;
+}
+
+void DynamicArray::setCapacite(const int nouvelle_capacite)
+{
+	int * tabTemporaire = new int[nouvelle_capacite];
+	for (int i = 0; i < nouvelle_capacite; i++)
+	{
+		tabTemporaire[i] = tabElement[i];
+	}
+}
+
+int DynamicArray::getElement(int i)
+{
+	return tabElement[i];
+}
+
+void DynamicArray::setElement(const int index_element, const int nouvelle_valeur_element)
+{
+	tabElement[index_element] = nouvelle_valeur_element;
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -6,12 +6,19 @@ class DynamicArray
 {
 
 public:
-
+	DynamicArray();
+	DynamicArray(const int &_capacite);
+	~DynamicArray(void);
+	int getCapacite();
+	void setCapacite(const int nouvelle_capacite);
+	int getElement(int i);
+	void setElement(const int index_element, const int nouvelle_valeur_element);
 
 
 private:
 
-
+	int capacite;
+	int* tabElement;
 };
 
 
